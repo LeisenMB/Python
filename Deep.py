80b285dc93449a2eecf89885ef3d372472dc3422
LeisenMB <115800789+LeisenMB@users.noreply.github.com>
2023-12-26 10:53:42 -0300

    Create Deep.py
    Añadir validación de respuestas para la Gran Pregunta de la Vida, el Universo y Todo lo Demás

---
+++ Deep.py
@@ -0,0 +1,6 @@
+n = str(input("What is the Answer to the Great Question of Life, the Universe, and Everything? "))
+
+if n == "42" or n == "fourty-two" or n == "fourty two":
+    print ("yes")
+else:
+    print ("no")
